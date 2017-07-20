0e05d7a8539679d43d4a951412161d5f76baf8c1
Nikita Barinov <nikitabarinov2301@gmail.com>
2017-07-20 21:26:32 +0400

    Слайдер

---
+++ source/js/slider.js
@@ -0,0 +1,36 @@
+var slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i,
+      slides = document.getElementsByClassName("slider__slide"),
+      dots = document.getElementsByClassName("slider__dot");
+
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" slider__dot--active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " slider__dot--active";
+}
